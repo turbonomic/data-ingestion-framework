e93e2c5ad926fdcd74251dcd9d51b56736edb3b8
Travis CI User <travis@example.org>
2024-03-28 04:02:46 -0400

    Preparing for https://github.com/turbonomic/data-ingestion-framework

---
+++ go.mod
@@ -0,0 +1,64 @@
+module github.ibm.com/turbonomic/data-ingestion-framework
+
+go 1.21 //Make sure you sync the change to the file Dockerfile.multi-archs after you update the version here
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/deckarep/golang-set v1.7.1
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/golang/glog v1.0.0
+	github.com/golang/protobuf v1.5.3
+	github.com/spf13/viper v1.16.0
+	github.com/stretchr/testify v1.8.3
+	github.com/tamerh/jsparser v1.2.0
+	github.ibm.com/turbonomic/turbo-go-sdk v0.0.0-20240228201902-4b7b10eeda01
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/apimachinery v0.23.5
+	k8s.io/client-go v0.23.5
+)
+
+require (
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/gnostic v0.5.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.17.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.ibm.com/turbonomic/turbo-api v0.0.0-20230919200445-2af52413d5a3 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.23.5 // indirect
+	k8s.io/klog/v2 v2.30.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
+	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
+	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
+
+replace golang.org/x/net => golang.org/x/net v0.17.0
---
+++ pkg/discovery/discovery_client.go
@@ -0,0 +1,320 @@
+package discovery
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/conf"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/data"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/dtofactory"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/registration"
+	prometurboversion "github.ibm.com/turbonomic/data-ingestion-framework/version"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/probe"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/proto"
+)
+
+// Implements the TurboDiscoveryClient interface
+type DIFDiscoveryClient struct {
+	discoveryTargetParams *DiscoveryTargetParams
+	keepStandalone        bool
+	metricEndpoint        []string
+	supplyChainConfig     *conf.SupplyChainConfig
+}
+
+type EntityBuilderParams struct {
+	keepStandalone    bool
+	supplyChainConfig *conf.SupplyChainConfig
+}
+
+type DiscoveryTargetParams struct {
+	OptionalTargetAddress *string
+	OptionalTargetName    *string
+	TargetType            string
+	ProbeCategory         string
+}
+
+func NewDiscoveryClient(discoveryTargetParams *DiscoveryTargetParams, keepStandalone bool,
+	supplyChainConfig *conf.SupplyChainConfig) *DIFDiscoveryClient {
+
+	return &DIFDiscoveryClient{
+		discoveryTargetParams: discoveryTargetParams,
+		keepStandalone:        keepStandalone,
+		supplyChainConfig:     supplyChainConfig,
+	}
+}
+
+// Get the Account Values to create VMTTarget in the turbo server corresponding to this client
+func (d *DIFDiscoveryClient) GetAccountValues() *probe.TurboTargetInfo {
+	targetParams := d.discoveryTargetParams
+
+	targetAddr := ""
+	if targetParams.OptionalTargetAddress != nil {
+		targetAddr = *targetParams.OptionalTargetAddress
+	}
+
+	targetName := ""
+	if targetParams.OptionalTargetName != nil {
+		targetName = *targetParams.OptionalTargetName
+	}
+
+	// this field is used to reach the target
+	targetIdField := registration.TargetIdField
+	targetIdVal := &proto.AccountValue{
+		Key:         &targetIdField,
+		StringValue: &targetAddr,
+	}
+
+	//this field is used as name of the target for displaying in the UI
+	targetNameField := registration.TargetNameField
+	targetNameVal := &proto.AccountValue{
+		Key:         &targetNameField,
+		StringValue: &targetName,
+	}
+
+	//this field is used as probe version of the target for displaying in the UI
+	probeVersionField := registration.ProbeVersion
+	probeVersionVal := &proto.AccountValue{
+		Key:         &probeVersionField,
+		StringValue: &prometurboversion.Version,
+	}
+
+	accountValues := []*proto.AccountValue{
+		targetIdVal,
+		targetNameVal,
+		probeVersionVal,
+	}
+
+	targetInfo := probe.NewTurboTargetInfoBuilder(targetParams.ProbeCategory,
+		targetParams.TargetType,
+		registration.TargetIdField,
+		accountValues).
+		Create()
+
+	glog.V(2).Infof("Created target info - id field: '%s', address:%s, name:%s",
+		targetInfo.TargetIdentifierField(), targetAddr, targetName)
+
+	return targetInfo
+}
+
+// Validate the Target
+func (d *DIFDiscoveryClient) Validate(accountValues []*proto.AccountValue) (*proto.ValidationResponse, error) {
+	glog.V(2).Infof("Validating target %s", accountValues)
+
+	targetAddr, found := targetAddress(accountValues)
+	if !found {
+		description := fmt.Sprintf("No target address (%s) in account values %v",
+			registration.TargetIdField, accountValueKeyNames(accountValues))
+		return d.failValidation(description), nil
+	}
+	validationResponse := &proto.ValidationResponse{}
+
+	// validate metric endpoint address
+	var errors []error
+	endpoints := strings.Split(targetAddr, ",")
+	for _, ep := range endpoints {
+		err := ValidateMetricDataSource(ep)
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+	if len(errors) > 0 {
+		err := fmt.Errorf("%v", errors)
+		validationResponse = d.failValidationWithError(targetAddr, err)
+	}
+
+	return validationResponse, nil
+}
+
+// Discover the Target Topology
+func (d *DIFDiscoveryClient) Discover(accountValues []*proto.AccountValue) (*proto.DiscoveryResponse, error) {
+
+	glog.V(2).Infof("Discovering target %s", accountValues)
+	targetAddr, found := targetAddress(accountValues)
+	if !found {
+		description := fmt.Sprintf("No target address (%s) in account values %v",
+			registration.TargetIdField, accountValueKeyNames(accountValues))
+		return d.failDiscovery(description), nil
+	}
+
+	// Create datasources to get the metrics or topology
+	endpoints := strings.Split(targetAddr, ",")
+	glog.Infof("Metric endpoints %++v", endpoints)
+	var datasources []MetricDataSource
+
+	for _, ep := range endpoints {
+		datasource := CreateMetricDataSource(ep)
+		datasources = append(datasources, datasource)
+	}
+
+	// Get the data from all the sources
+	difResult, err := GetDIFData(datasources)
+	if err != nil {
+		glog.Errorf("err : %++v", err)
+		return d.failDiscoveryWithError(targetAddr, err), nil
+	}
+
+	glog.V(2).Infof("Number of parsed entities %d", len(difResult.ParsedEntities))
+
+	// Create the repository and entities
+	repository := data.NewDIFRepository()
+	repository.InitRepository(difResult.ParsedEntities)
+
+	for entityType, eMap := range repository.EntityMap {
+		for entityId, difEntity := range eMap {
+			glog.V(4).Infof("Entity %s::%s	-----> ", entityType, entityId)
+			for pType, pIds := range difEntity.GetProviders() {
+				glog.V(4).Infof("		provider %s [%v] ---> ", pType, pIds)
+			}
+			for pType, pMap := range difEntity.HostsByIP {
+				for pId := range pMap {
+					glog.V(4).Infof("		hostedBy %s [%v] ---> ", pType, pId)
+				}
+			}
+		}
+	}
+
+	// Build Entity DTOs
+	dtos, err := d.buildEntities(repository, difResult.Scope)
+	if err != nil {
+		return d.failDiscoveryWithError(targetAddr, err), nil
+	}
+
+	return &proto.DiscoveryResponse{EntityDTO: dtos}, nil
+}
+
+func accountValueKeyNames(accountValues []*proto.AccountValue) []*string {
+	names := make([]*string, len(accountValues))
+	for i := range accountValues {
+		names[i] = accountValues[i].Key
+	}
+	return names
+}
+
+// targetAddress reads the target address from the array of account values.
+// The first value returned is the address, if found.
+// The second value returned is a bool indicating whether or not the address was successfully found.
+func targetAddress(accountValues []*proto.AccountValue) (string, bool) {
+	return matchingAccountValue(accountValues, registration.TargetIdField)
+}
+
+func matchingAccountValue(accountValues []*proto.AccountValue, matchKey string) (string, bool) {
+	for _, value := range accountValues {
+		if *value.Key == matchKey {
+			return *value.StringValue, true
+		}
+	}
+
+	return "", false
+}
+
+func (d *DIFDiscoveryClient) buildEntities(repository *data.DIFRepository, scope string) ([]*proto.EntityDTO, error) {
+
+	var entities []*proto.EntityDTO
+
+	// Create the supply chain nodes using the config
+	supplyChain, err := registration.NewSupplyChain(d.supplyChainConfig)
+	if err != nil {
+		return entities, err
+	}
+
+	idToEntityMap := make(map[string]*data.BasicDIFEntity)
+	supplyChainNodeMap := supplyChain.GetSupplyChainNodes()
+	// Build entity DTOs using the corresponding supply chain template
+	for difEntityType, eMap := range repository.EntityMap {
+		for difEntityId, difEntity := range eMap {
+			// entity type
+			eType := dtofactory.EntityType(difEntityType)
+			if eType == nil {
+				glog.Errorf("Invalid entity type %v", difEntity)
+				continue
+			}
+			entityType := *eType
+
+			supplyChainNode, validType := supplyChainNodeMap[entityType]
+			if !validType {
+				glog.Errorf("Supply chain does not support entity type %v", difEntity)
+				continue
+			}
+			if existingEntity, seen := idToEntityMap[difEntityId]; seen {
+				// Entities with the same ID and same type will be merged. Entities with the same ID but different
+				// types will be rejected.
+				glog.Errorf("Duplicated entity ID detected with different entity types. Entity ID: %v,"+
+					" Entity types: %v, %v", difEntityId, existingEntity.EntityType, difEntityType)
+				continue
+			}
+			ab := dtofactory.NewGenericEntityBuilder(entityType, difEntity, scope,
+				d.keepStandalone, supplyChainNode)
+			dto, err := ab.BuildEntity()
+			if err != nil {
+				glog.Errorf("Error building entity %s::%s %++v", difEntityType, difEntityId, err)
+				continue
+			}
+			idToEntityMap[difEntityId] = difEntity
+			entities = append(entities, dto)
+		}
+	}
+
+	// create proxy providers
+	for providerType, pIds := range repository.ExternalProxyProvidersByIP {
+		eType := dtofactory.EntityType(providerType)
+		if eType == nil {
+			glog.Errorf("Invalid entity type %v", providerType)
+			continue
+		}
+		entityType := *eType
+
+		supplyChainNode, validType := supplyChainNodeMap[entityType]
+		if !validType {
+			glog.Errorf("Supply chain does not support external provider entity type %v", entityType)
+			continue
+		}
+
+		for _, pId := range pIds {
+			pp := dtofactory.NewProxyProviderEntityBuilder(entityType, pId, scope, d.keepStandalone, supplyChainNode)
+			dto, err := pp.BuildEntity()
+			if err != nil {
+				glog.Errorf("Error building entity %s::%s %++v", providerType, pId, err)
+				continue
+			}
+			entities = append(entities, dto)
+		}
+	}
+
+	return entities, nil
+}
+
+func (d *DIFDiscoveryClient) failDiscoveryWithError(targetAddr string, err error) *proto.DiscoveryResponse {
+	return d.failDiscovery(fmt.Sprintf("Discovery of %s failed due to error: %v", targetAddr, err))
+}
+
+func (d *DIFDiscoveryClient) failDiscovery(description string) *proto.DiscoveryResponse {
+	glog.Errorf(description)
+	severity := proto.ErrorDTO_CRITICAL
+	errorDTO := &proto.ErrorDTO{
+		Severity:    &severity,
+		Description: &description,
+	}
+	discoveryResponse := &proto.DiscoveryResponse{
+		ErrorDTO: []*proto.ErrorDTO{errorDTO},
+	}
+	return discoveryResponse
+}
+
+func (d *DIFDiscoveryClient) failValidationWithError(targetAddr string, err error) *proto.ValidationResponse {
+	return d.failValidation(fmt.Sprintf("Validation of %s failed due to error: %v", targetAddr, err))
+}
+
+func (d *DIFDiscoveryClient) failValidation(description string) *proto.ValidationResponse {
+	glog.Errorf(description)
+	severity := proto.ErrorDTO_CRITICAL
+	errorDto := &proto.ErrorDTO{
+		Severity:    &severity,
+		Description: &description,
+	}
+
+	validationResponse := &proto.ValidationResponse{
+		ErrorDTO: []*proto.ErrorDTO{errorDto},
+	}
+	return validationResponse
+}
