5ef5f0302bc465accffdf6db059779e2814ccad6
Travis CI User <travis@example.org>
2024-12-09 03:02:25 -0500

    Preparing for https://github.com/turbonomic/data-ingestion-framework

---
+++ pkg/dtofactory/util.go
@@ -0,0 +1,95 @@
+package dtofactory
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/golang/glog"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/data"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/registration"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/proto"
+)
+
+func EntityType(difType data.DIFEntityType) *proto.EntityDTO_EntityType {
+	entityTypeStr := data.DIFEntityTypeToTemplateEntityStringMap[difType]
+	eType, exists := registration.TemplateEntityTypeMap[entityTypeStr]
+	if !exists {
+		return nil
+	}
+
+	return &eType
+}
+
+func getKey(entityType proto.EntityDTO_EntityType, entityName, scope string) string {
+	eType := proto.EntityDTO_EntityType_name[int32(entityType)]
+	return fmt.Sprintf("%s-%s-%s", eType, entityName, scope)
+}
+
+func getProxyEntityId(entityType proto.EntityDTO_EntityType, entityId, scope string) string {
+	hasher := sha1.New()
+	hasher.Write([]byte(fmt.Sprintf("%s%s%s", entityType, entityId, scope)))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func getEntityPropertyNameValue(name, value string) *proto.EntityDTO_EntityProperty {
+	attr := name
+	ns := registration.DefaultPropertyNamespace
+
+	return &proto.EntityDTO_EntityProperty{
+		Namespace: &ns,
+		Name:      &attr,
+		Value:     &value,
+	}
+}
+
+func logDebug(msg ...interface{}) {
+	if os.Getenv("TURBODIF_LOCAL_DEBUG") == "1" && glog.V(4) {
+		glog.Infof("%+v", msg)
+	}
+}
+
+func logSupplyChainDetails(supplyChainNode *registration.SupplyChainNode) {
+	if os.Getenv("TURBODIF_LOCAL_DEBUG") == "1" && glog.V(4) {
+
+		var expectedSoldComms []string
+		for comm := range supplyChainNode.SupportedComms {
+			expectedSoldComms = append(expectedSoldComms, fmt.Sprintf("%v", comm))
+		}
+		glog.V(4).Infof("expectedSoldComms: %v", expectedSoldComms)
+
+		var expectedSoldAccessComms []string
+		for comm := range supplyChainNode.SupportedAccessComms {
+			expectedSoldAccessComms = append(expectedSoldAccessComms, fmt.Sprintf("%v", comm))
+		}
+		glog.V(4).Infof("expectedSoldAccessComms: %v", expectedSoldAccessComms)
+
+		expectedBought := make(map[string][]string)
+		for provider, bought := range supplyChainNode.SupportedBoughtComms {
+			var comms []string
+			for comm := range bought {
+				comms = append(comms, fmt.Sprintf("%v", comm))
+			}
+			expectedBought[fmt.Sprintf("%v", provider)] = comms
+		}
+		glog.V(4).Infof("expectedBought: %v", expectedBought)
+
+		expectedAccessBought := make(map[string][]string)
+		for provider, bought := range supplyChainNode.SupportedBoughtAccessComms {
+			var comms []string
+			for comm := range bought {
+				comms = append(comms, fmt.Sprintf("%v", comm))
+			}
+			expectedAccessBought[fmt.Sprintf("%v", provider)] = comms
+		}
+		glog.V(4).Infof("expectedAccessBought: %v", expectedAccessBought)
+
+		hostedByProviderType := supplyChainNode.ProviderByProviderType
+		expectedHostedByProviderType := make(map[string]string)
+		for provider, hostingType := range hostedByProviderType {
+			expectedHostedByProviderType[fmt.Sprintf("%v", provider)] = hostingType
+		}
+		glog.V(4).Infof("expectedHostedByProviderType: %v", expectedHostedByProviderType)
+	}
+}
