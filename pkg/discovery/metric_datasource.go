c221ee27b8f33cd996b168809bf513633f696387
Travis CI User <travis@example.org>
2024-12-21 03:02:12 -0500

    Preparing for https://github.com/turbonomic/data-ingestion-framework

---
+++ pkg/discovery/metric_datasource.go
@@ -0,0 +1,248 @@
+package discovery
+
+import (
+	"bufio"
+	"bytes"
+
+	"github.com/tamerh/jsparser"
+
+	//"bufio"
+	//"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/dataingestionframework/data"
+)
+
+const (
+	FILE_URL = "file://"
+	HTTP_URL = "http://"
+)
+
+type MetricDataSource interface {
+	GetMetricData() (*data.Topology, error)
+	GetMetricEndpoint() string
+}
+
+func ValidateMetricDataSource(metricEndpoint string) error {
+	metricEndpoint = strings.TrimSpace(metricEndpoint)
+
+	var valid bool
+	switch {
+	// HTTP endpoint - send request and retrieve the JSON response
+	case strings.HasPrefix(metricEndpoint, HTTP_URL):
+		valid = true
+	// File based
+	case strings.HasPrefix(metricEndpoint, FILE_URL):
+		valid = true
+	default:
+		valid = false
+		glog.Errorf("Unsupported metric endpoint: %s", metricEndpoint)
+	}
+
+	if valid {
+		return nil
+	}
+	return fmt.Errorf("unsupported metric endpoint: %s", metricEndpoint)
+}
+
+func CreateMetricDataSource(metricEndpoint string) MetricDataSource {
+	var metricDataSource MetricDataSource
+	metricEndpoint = strings.TrimSpace(metricEndpoint)
+
+	switch {
+	// HTTP endpoint - send request and retrieve the JSON response
+	case strings.HasPrefix(metricEndpoint, HTTP_URL):
+		metricDataSource = NewHTTPBasedMetricDatSource(metricEndpoint)
+	// File based
+	case strings.HasPrefix(metricEndpoint, FILE_URL):
+		metricDataSource = NewHFileBasedMetricDatSource(metricEndpoint)
+	default:
+		glog.Errorf("Unsupported metric endpoint: %s", metricEndpoint)
+	}
+
+	return metricDataSource
+}
+
+type HTTPBasedMetricDatSource struct {
+	metricsUrl string
+}
+
+func NewHTTPBasedMetricDatSource(metricEndpoint string) *HTTPBasedMetricDatSource {
+	return &HTTPBasedMetricDatSource{
+		metricsUrl: metricEndpoint,
+	}
+}
+func (md *HTTPBasedMetricDatSource) GetMetricEndpoint() string {
+	return md.metricsUrl
+}
+
+func (md *HTTPBasedMetricDatSource) GetMetricData() (*data.Topology, error) {
+	params := map[string]string{}
+	var resp []byte
+	resp, err := sendRequest(md.metricsUrl, params)
+	if err != nil {
+		return nil, err
+	}
+
+	//topology, err := loadJSON(md.metricsUrl, resp)
+	topology, err := loadJSONStream(md.metricsUrl, resp)
+
+	return topology, nil
+}
+
+// Send a request to the given endpoint. Params are encoded as query parameters
+func sendRequest(endpoint string, params map[string]string) ([]byte, error) {
+	url, err := encodeRequest(endpoint, params)
+	if err != nil {
+		return nil, err
+	}
+
+	glog.V(2).Infof("Sending request to %s", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		glog.Errorf("Failed getting response from %s: %v", url, err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Errorf("Error reading the response %v: %v", resp, err)
+		return nil, err
+	}
+	glog.V(4).Infof("Received response: %s", string(body))
+	return body, nil
+}
+
+func encodeRequest(endpoint string, params map[string]string) (string, error) {
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return "", err
+	}
+
+	q := req.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+	return req.URL.String(), nil
+}
+
+type FileBasedMetricDatSource struct {
+	metricsFile string
+}
+
+func (md *FileBasedMetricDatSource) GetMetricEndpoint() string {
+	return md.metricsFile
+}
+
+func NewHFileBasedMetricDatSource(metricEndpoint string) *FileBasedMetricDatSource {
+	return &FileBasedMetricDatSource{
+		metricsFile: metricEndpoint,
+	}
+}
+
+func (md *FileBasedMetricDatSource) GetMetricData() (*data.Topology, error) {
+	var resp []byte
+	filename := string(md.metricsFile[7:])
+	resp, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("file error: %v", err.Error())
+	}
+
+	topology, err := loadJSON(md.metricsFile, resp)
+
+	return topology, err
+}
+
+func loadJSON(metricEndpoint string, resp []byte) (*data.Topology, error) {
+
+	var topology *data.Topology
+	err := json.Unmarshal(resp, &topology)
+	if err != nil {
+		glog.Errorf("Topology JSON unmarshall error: %v\n", err)
+		return nil, fmt.Errorf("json unmarshall error: %v", err.Error())
+	}
+	if topology.Entities != nil {
+		glog.Infof("%s: parsed %d entities\n", metricEndpoint, len(topology.Entities))
+	}
+
+	return topology, nil
+}
+
+func loadJSONStream(metricEndpoint string, resp []byte) (*data.Topology, error) {
+
+	rb := bytes.NewBuffer(resp)
+	reader := bufio.NewReader(rb)
+	parser := jsparser.NewJSONParser(reader, "scope")
+	var scope string
+	for json := range parser.Stream() {
+		glog.Infof("Scope %+v", json.StringVal)
+		scope = json.StringVal
+	}
+
+	rb = bytes.NewBuffer(resp)
+	reader = bufio.NewReader(rb)
+	parser = jsparser.NewJSONParser(reader, "topology")
+	var entities []*data.DIFEntity
+	for json := range parser.Stream() {
+		// Create DIFEntity
+		entity := &data.DIFEntity{
+			UID:                 json.ObjectVals["uniqueId"].StringVal,
+			Type:                json.ObjectVals["type"].StringVal,
+			Name:                json.ObjectVals["name"].StringVal,
+			HostedOn:            nil,
+			MatchingIdentifiers: nil,
+			PartOf:              nil,
+			Metrics:             nil,
+			ProviderMustClone:   json.ObjectVals["providerMustClone"].BoolVal,
+			Controllable:        json.ObjectVals["controllable"].BoolVal,
+			Cloneable:           json.ObjectVals["cloneable"].BoolVal,
+			Suspendable:         json.ObjectVals["suspendable"].BoolVal,
+		}
+		// ----- Matching Identifiers
+		entity.MatchingIdentifiers = parseMatchingIdentifiers(json)
+
+		// ----- Part Of
+		entity.PartOf = parsePartOf(json)
+
+		// ----- HostedOn
+		entity.HostedOn = parseHostedOn(json)
+
+		// ------ Metrics
+		if json.ObjectVals["metrics"] != nil {
+			difMetricValMap := make(map[string][]*data.DIFMetricVal)
+
+			metricsMap := json.ObjectVals["metrics"].ObjectVals
+			for metricName, metrics := range metricsMap {
+				metricList := parseMetricVal(metrics)
+				difMetricValMap[metricName] = metricList
+			}
+			entity.Metrics = difMetricValMap
+		}
+		glog.V(4).Infof("%v", entity)
+		entities = append(entities, entity)
+	} //end of topology
+
+	var topology *data.Topology
+	topology = &data.Topology{
+		Version:    "",
+		Updatetime: 0,
+		Scope:      scope,
+		Source:     scope,
+		Entities:   entities,
+	}
+
+	if topology.Entities != nil {
+		glog.Infof("%s: parsed %d entities", metricEndpoint, len(topology.Entities))
+	}
+	return topology, nil
+
+}
---
+++ pkg/dtofactory/commodity_builder.go
@@ -0,0 +1,115 @@
+package dtofactory
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/data"
+	"github.ibm.com/turbonomic/data-ingestion-framework/pkg/registration"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/builder"
+	difdata "github.ibm.com/turbonomic/turbo-go-sdk/pkg/dataingestionframework/data"
+	"github.ibm.com/turbonomic/turbo-go-sdk/pkg/proto"
+)
+
+type GenericCommodityBuilder struct {
+	entity *difdata.DIFEntity
+}
+
+type CommoditiesByType map[proto.CommodityDTO_CommodityType][]*builder.CommodityDTOBuilder
+
+func NewGenericCommodityBuilder(entity *difdata.DIFEntity) *GenericCommodityBuilder {
+	return &GenericCommodityBuilder{entity: entity}
+}
+
+func (cb *GenericCommodityBuilder) BuildCommodity() (CommoditiesByType, error) {
+	result := make(CommoditiesByType)
+
+	metrics := cb.entity.Metrics
+	for metricKey, metricList := range metrics { // Metrics is array of metric map [name,metric Value]
+		// Check is metric is supported
+		metricName := data.DIFMetricToTemplateCommodityStringMap[metricKey]
+		commodityType, exists := registration.TemplateCommodityTypeMap[metricName]
+		if !exists {
+			glog.Errorf("%s:%s data has unsupported metric %s",
+				cb.entity.Type, cb.entity.UID, metricName)
+			continue
+		}
+
+		commodities, err := convertFromMetricValueListToCommodityList(commodityType, metricList)
+
+		if err != nil {
+			glog.Errorf("%v", err)
+		}
+
+		result[commodityType] = commodities
+	}
+	return result, nil
+}
+
+func convertFromMetricValueListToCommodityList(commType proto.CommodityDTO_CommodityType,
+	responseMetrics []*difdata.DIFMetricVal) ([]*builder.CommodityDTOBuilder, error) { //([]*proto.CommodityDTO, error) {
+
+	var commodityList []*builder.CommodityDTOBuilder //[]*proto.CommodityDTO
+
+	for _, responseMetric := range responseMetrics {
+		if responseMetric.Average == nil {
+			return nil, fmt.Errorf("invalid commodity, missing average value for %v", commType)
+		}
+		commBuilder := builder.NewCommodityDTOBuilder(commType)
+		if responseMetric.Capacity != nil {
+			capacity := *responseMetric.Capacity
+			commBuilder.Capacity(capacity) //TODO: do not set for bought commodity
+		}
+		if responseMetric.Average != nil {
+			average := *responseMetric.Average
+			commBuilder.Used(average)
+		}
+		if responseMetric.Key != nil {
+			commKey := *responseMetric.Key
+			commBuilder.Key(commKey)
+		}
+		if responseMetric.Resizable != nil {
+			commBuilder.Resizable(*responseMetric.Resizable)
+		}
+
+		commodityList = append(commodityList, commBuilder)
+	}
+	return commodityList, nil
+}
+
+func setResizable(entityType proto.EntityDTO_EntityType, commMap CommoditiesByType) {
+	// We must explicitly set resizable to false on an non-resizable commodity
+	for _, nonResizableCommodity := range registration.NonResizableCommodities {
+		if commList, found := commMap[nonResizableCommodity]; found {
+			for _, comm := range commList {
+				comm.Resizable(false)
+			}
+		}
+	}
+	switch entityType {
+	case proto.EntityDTO_APPLICATION_COMPONENT:
+		if commList, foundHeap := commMap[proto.CommodityDTO_HEAP]; foundHeap {
+			resizable := false
+			if _, foundGC := commMap[proto.CommodityDTO_REMAINING_GC_CAPACITY]; foundGC {
+				resizable = true
+			}
+			for _, comm := range commList {
+				if !comm.HasResizable() {
+					comm.Resizable(resizable)
+				}
+			}
+		}
+	case proto.EntityDTO_DATABASE_SERVER:
+		if commList, foundHeap := commMap[proto.CommodityDTO_DB_MEM]; foundHeap {
+			resizable := false
+			if _, foundGC := commMap[proto.CommodityDTO_DB_CACHE_HIT_RATE]; foundGC {
+				resizable = true
+			}
+			for _, comm := range commList {
+				if !comm.HasResizable() {
+					comm.Resizable(resizable)
+				}
+			}
+		}
+	}
+}
