8d7ffced22f5ba777b6b3bc6ccd91d4176621c79
Travis CI User <travis@example.org>
2025-01-03 03:02:41 -0500

    Preparing for https://github.com/turbonomic/data-ingestion-framework

---
+++ pkg/dtofactory/constants.go
@@ -0,0 +1,10 @@
+package dtofactory
+
+const (
+	// Entity property name for IP address
+	IPAttr                       = "IP"
+	KubernetesFullyQualifiedName = "KubernetesFullyQualifiedName"
+
+	HOSTING      = "HOSTING"
+	LAYERED_OVER = "LAYERED_OVER"
+)
